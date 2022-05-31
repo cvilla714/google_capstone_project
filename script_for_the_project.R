8cc6fe90f620741b479213e3230691997aa08b58
Cosmel <cosmelvg@gmail.com>
2022-05-31 11:05:38 -0600

    Update script_for_the_project
    - Add several commands to display
      - structure of the data frama
      - dimension (dimension of the data frame)
      - head (pulling the first 6 rows of the data frame)
      - tail (pulling the last 6 rows of the data frame)
      - summary (get statistical summary of the whole data frame)
      - table (get the total number of customers per label)
    - Add new columns to the data frame
      - date
      - month
      - day
      - year
      - day of the week
      - ride length (contains the lenght in seconds of each ride)
    - Add new variable called all_trips_v2 to hold all new information

--- script_for_the_project.R
+++ script_for_the_project.R
@@ -74,6 +74,67 @@ str(all_trips)
 all_trips <- all_trips %>%  
   select(-c(start_lat, start_lng, end_lat, end_lng))
 
-#listing new structure and columns from the new data frame
+
+
+#======================================================
+# STEP 3: CLEAN UP AND ADD DATA TO PREPARE FOR ANALYSIS
+#======================================================
+#listing all columns from the new data frame
 colnames(all_trips)
+
+#display the structure of the new data frame
 str(all_trips)
+
+#dislay the dimensions of the data frame
+dim(all_trips)
+
+#show the first 6 rows of the data frame
+head(all_trips)
+
+#show the last 6 rows of the data frame
+tail(all_trips)
+
+#displays the statistical summary of the whole data frame.
+summary(all_trips)
+
+#checking the amount of users per labe
+table(all_trips$member_casual)
+
+# Add columns that list the date, month, day, and year of each ride
+#The default format is yyyy-mm-dd
+all_trips$date <- as.Date(all_trips$started_at)
+all_trips$month <- format(as.Date(all_trips$date), "%m")
+all_trips$day <- format(as.Date(all_trips$date), "%d")
+all_trips$year <- format(as.Date(all_trips$date), "%Y")
+all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
+
+
+str(all_trips)
+
+# Add a "ride_length" calculation to all_trips (in seconds)
+all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at)
+
+str(all_trips)
+
+
+# Convert "ride_length" from Factor to numeric so we can run calculations on the data
+is.factor(all_trips$ride_length)
+all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
+is.numeric(all_trips$ride_length)
+
+
+# Remove "bad" data
+all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$ride_length<0),]
+
+#several commands to run to check
+#check structure of the data frame
+str(all_trips_v2)
+
+#look at the first 6 rows of the data frame
+head(all_trips_v2)
+
+#look at the last 6 rows of the data frame
+tail(all_trips_v2)
+
+#looks at column names
+colnames(all_trips_v2)
