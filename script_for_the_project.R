6f2f6e7513435f171fe47d87e360ee63de42f246
Cosmel <cosmelvg@gmail.com>
2022-05-31 10:05:06 -0600

    Update script_for_the_project
    - Add variables to read the information from each file and store it

--- script_for_the_project.R
+++ script_for_the_project.R
@@ -12,3 +12,21 @@ library(ggplot2)
 
 #get my working directory
 getwd()
+
+
+#=====================
+# STEP 1: COLLECT DATA
+#=====================
+# Upload Divvy datasets (csv files)
+m1_2021 <- read.csv("202101-divvy-tripdata.csv")
+m2_2021 <- read.csv("202102-divvy-tripdata.csv")
+m3_2021 <- read.csv("202103-divvy-tripdata.csv")
+m4_2021 <- read.csv("202104-divvy-tripdata.csv")
+m5_2021 <- read.csv("202105-divvy-tripdata.csv")
+m6_2021 <- read.csv("202106-divvy-tripdata.csv")
+m7_2021 <- read.csv("202107-divvy-tripdata.csv")
+m8_2021 <- read.csv("202108-divvy-tripdata.csv")
+m9_2021 <- read.csv("202109-divvy-tripdata.csv")
+m10_2021 <- read.csv("202110-divvy-tripdata.csv")
+m11_2021 <- read.csv("202111-divvy-tripdata.csv")
+m12_2021 <- read.csv("202112-divvy-tripdata.csv")
