09333bb9e98a827d3766ff20e317a47144bbc8bd
Cosmel <cosmelvg@gmail.com>
2022-05-31 09:48:50 -0600

    Update script_for_the_project
    - Include initial commands to
      - install packages
      - load packages

---
+++ script_for_the_project.R
@@ -0,0 +1,14 @@
+#Adding instructions for the scrip
+
+#Installing packages
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("ggplot2")
+
+#loading packages
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+
+#get my working directory
+getwd()
