1a8df113389e81559e96f4a9f27f310a7478725e
Cosmel <cosmelvg@gmail.com>
2022-05-31 10:36:02 -0600

    Update script_for_the_project
    - List all columns and structure from each data frame
    - Combine all data frame into a single one
    - Remove uncessary columns:
      - start_lat
      - start_lng
      - end_lat
      - end_lng
    - List the columns from new data frame and structure

--- script_for_the_project.R
+++ script_for_the_project.R
@@ -30,3 +30,50 @@ m9_2021 <- read.csv("202109-divvy-tripdata.csv")
 m10_2021 <- read.csv("202110-divvy-tripdata.csv")
 m11_2021 <- read.csv("202111-divvy-tripdata.csv")
 m12_2021 <- read.csv("202112-divvy-tripdata.csv")
+
+
+#====================================================
+# STEP 2: WRANGLE DATA AND COMBINE INTO A SINGLE FILE
+#====================================================
+colnames(m1_2021)
+colnames(m2_2021)
+colnames(m3_2021)
+colnames(m4_2021)
+colnames(m5_2021)
+colnames(m6_2021)
+colnames(m7_2021)
+colnames(m8_2021)
+colnames(m9_2021)
+colnames(m10_2021)
+colnames(m11_2021)
+colnames(m12_2021)
+
+
+# Inspect the dataframes and look for incongruencies
+str(m1_2021)
+str(m2_2021)
+str(m3_2021)
+str(m4_2021)
+str(m5_2021)
+str(m6_2021)
+str(m7_2021)
+str(m8_2021)
+str(m9_2021)
+str(m10_2021)
+str(m11_2021)
+str(m12_2021)
+
+# Stack individual quarter's data frames into one big data frame
+
+all_trips <- bind_rows(m1_2021,m2_2021,m3_2021,m4_2021,m5_2021,m6_2021,m7_2021,m8_2021,m9_2021,m10_2021,m11_2021,m12_2021)
+
+colnames(all_trips)
+str(all_trips)
+
+# Remove lat, long fields as this data was dropped beginning in 2020
+all_trips <- all_trips %>%  
+  select(-c(start_lat, start_lng, end_lat, end_lng))
+
+#listing new structure and columns from the new data frame
+colnames(all_trips)
+str(all_trips)
